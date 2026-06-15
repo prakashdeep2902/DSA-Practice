2da3645e59900e02b89b203cfdb64921030d9270
sharmaJi <prakashdeep2902@gmail.com>
2026-06-15 08:23:53 +0530

    Two-pointers

---
+++ Two-pointers.js
@@ -0,0 +1,64 @@
+/*  
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets. 
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+*/
+
+// soloved answer
+
+function threeSum(nums) {
+  const arr = nums.sort((a, b) => a - b);
+  const result = [];
+  const n = arr.length;
+  for (let i = 0; i < n - 2; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    let left = i + 1;
+    let right = n - 1;
+    const target = -arr[i];
+    while (left < right) {
+      const sum = arr[left] + arr[right];
+      if (sum === target) {
+        result.push([arr[i], arr[left], arr[right]]);
+        left++;
+        right--;
+        while (left < n && arr[left] === arr[left - 1]) {
+          left++;
+        }
+        while (right >= 0 && arr[right] === arr[right + 1]) {
+          right--;
+        }
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
